4ac9110088025a9fbcc1973fe64acb2b92e22549
PC-trollbox <71093117+PC-trollbox@users.noreply.github.com>
2025-01-31 21:12:42 +0300

    fix installer not loading fs changes

--- os_system/combine_into_index/boot/00-pcos.js
+++ os_system/combine_into_index/boot/00-pcos.js
@@ -1,6 +1,6 @@
 // @pcos-app-mode native
-const pcos_version = "1165";
-const build_time = 1738346992595;
+const pcos_version = "1166";
+const build_time = 1738347147544;
  
 let modules = {
 	core: coreExports,
