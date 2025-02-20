e06a36829975eef27db3c7c3a1997cdcc3429454
PC-trollbox <71093117+PC-trollbox@users.noreply.github.com>
2025-02-20 20:25:48 +0300

    move some icons to vector (51 KB reduction)

--- os_system/combine_into_index/boot/00-pcos.js
+++ os_system/combine_into_index/boot/00-pcos.js
@@ -1,6 +1,6 @@
 // @pcos-app-mode native
-const pcos_version = "1196";
-const build_time = 1739896845786;
+const pcos_version = "1197";
+const build_time = 1740072181462;
  
 let modules = {
 	core: coreExports,
